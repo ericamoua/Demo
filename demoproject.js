76a1580136d799077ae93a3deb92a578985a598e
Erica Moua <erica.moua568@gmail.com>
2023-12-09 20:04:18 -0500

    Fixed repo

---
+++ demoproject.js
@@ -0,0 +1,14 @@
+//Practice loops 
+console.log('Hello World');
+
+for (let counter = 4; counter <= 6; counter ++){
+    console.log(counter);
+}
+
+for (let counter = 2; counter >= 2; counter ++){
+    console.log(counter);
+}
+
+for (let counter = 9; counter >= 5; counter ++){
+    console.log(counter);
+}
